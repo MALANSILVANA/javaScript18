b73238b1efe626755aaccaeccf8065cf3409c5a5
MALANSILVANA <silvana.malan@epn.edu.ec>
2018-04-25 16:06:07 -0500

    operadores

---
+++ typesScript/02-operadores.js
@@ -0,0 +1,43 @@
+var arregloNumerosUno = [1, 2, 3, 4, 5];
+var ArregloUsuarios;
+nombre: 'carlos',
+    edad;
+28;
+;
+//let arregloNumerosDos:Array<number>=[1,2,3,4,5];
+//funcion anonima
+var sumarDosNumeros = function (numeroUno, numeroDos) {
+    //flecha gorda
+    return numeroUno + numeroDos;
+};
+var usuriosCasados = arregloUsuarios
+    .map();
+var potenciaDeUnNumero = function (numero) {
+    return numero * numero;
+};
+/*
+let potenciaDeUnNumero = numero =>numero*numero {
+* */
+var sumatoriaDeValores = 0;
+var resultadoForEach = arregloNumerosUno.forEach(function (valorDelArreglo, indice, arreglo) {
+    console.log(valorDelArreglo);
+    console.log(indice);
+    console.log(arreglo);
+}, console.log('Resultado forEach', resultadoForEach));
+console.log('sumatoria de valores', sumatoriaDeValores);
+var resultadoSuma = arregloNumerosUno
+    .reduce(function (valorActualDeLaOperacion, valorDelArreglo) {
+    console.log('valorActualDeLaOperacion', valorActualDeLaOperacion);
+    console.log('valorDelArreglo', valorDelArreglo);
+    return valorActualDeLaOperacion + valorDelArreglo;
+}, 20);
+some(//or
+function (usuario) {
+    return usuario.edad;
+}, console.log('ResultadoSuma', resultadoSuma));
+//.filter{
+function (usuario) { }
+{
+    return (usuario.deuda <= 310);
+}
+;
---
+++ typesScript/02-operadores.ts
@@ -0,0 +1,59 @@
+let arregloNumerosUno:number[]=[1,2,3,4,5];
+let ArregloUsuarios : [
+    {
+        nombre: 'carlos',
+        edad:28};
+nombre: 'carlos',
+    edad:28};
+    ]
+
+//let arregloNumerosDos:Array<number>=[1,2,3,4,5];
+//funcion anonima
+let sumarDosNumeros =
+    (numeroUno:number, numeroDos: number): number =>{ //funciones de
+    //flecha gorda
+    return numeroUno + numeroDos;
+    };
+const usuriosCasados = arregloUsuarios
+    .map()
+let potenciaDeUnNumero = (numero) => {
+    return numero*numero;
+};
+/*
+let potenciaDeUnNumero = numero =>numero*numero {
+* */
+let sumatoriaDeValores = 0;
+let resultadoForEach = arregloNumerosUno.forEach(
+ (valorDelArreglo, indice, arreglo)=> {
+    console.log(valorDelArreglo);
+    console.log(indice);
+    console.log(arreglo);
+},
+console.log('Resultado forEach', resultadoForEach);
+console.log('sumatoria de valores', sumatoriaDeValores);
+var resultadoSuma = arregloNumerosUno
+    .reduce(
+        (valorActualDeLaOperacion, valorDelArreglo)=>{
+            console.log('valorActualDeLaOperacion',
+                valorActualDeLaOperacion);
+            console.log('valorDelArreglo',
+                valorDelArreglo);
+            return valorActualDeLaOperacion + valorDelArreglo
+        },
+       20
+);
+.some (//or
+    (usuario: UsuarioArreglo)=>{
+        return usuario.edad
+    }
+console.log('ResultadoSuma', resultadoSuma);
+
+//.filter{
+    function (usuario: UsuarioArreglo)=> {
+        return (usuario.deuda <= 310);
+
+    };
+}
+interface Interface {
+    
+}
